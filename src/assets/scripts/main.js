a3edbc294f57e13a4cb6053d56de4e0ce3888853
Daiana Varela <dvarelaal@uoc.edu>
2026-05-12 18:23:52 +0200

    First commit

---
+++ src/assets/scripts/main.js
@@ -0,0 +1,31 @@
+/**
+ * Import dependencies from node_modules
+ * see commented examples below
+ */
+
+// import 'some-node-module';
+// import SomeModule from 'some-node-module';
+
+import '@fontsource-variable/montserrat/wght.css';
+import '@fontsource/archivo-black';
+
+
+
+// Import all of Bootstrap’s JS
+import * as bootstrap from 'bootstrap';
+
+
+
+import { gsap } from "gsap";
+    
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { SplitText } from "gsap/SplitText";
+
+gsap.registerPlugin(ScrollTrigger,SplitText);
+
+
+/**
+ * Write any other JavaScript below
+ */
+
+
